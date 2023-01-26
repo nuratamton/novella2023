be53b26fd007a1893e5029aeef5a7be2b7682d57
Nura Tamton <nrt2000@hw.ac.uk>
2023-01-26 22:16:26 +0400

    implemented search feature

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,105 +1,95 @@
+import { SafeAreaView, StyleSheet, Text, View, FlatList, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
 import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  TouchableOpacity,
-} from "react-native";
-import React, { useState } from "react";
-import { Button, SearchBar } from "react-native-elements";
-import { FlatList, ScrollView } from "react-native-gesture-handler";
-// import filter from "lodash.filter";
+  getDocs,
+  getDoc,
+  collection,
+  doc,
+  setDoc,
+  collectionGroup,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { db, auth } from "../firebase";
+import InputBox from "../components/InputBox";
 
 const Explore = () => {
-  const users = [
-    {
-      id: "1",
-      userName: "nuratamton",
-    },
-    {
-      id: "2",
-      userName: "gaurangchitnis",
-    },
-    {
-      id: "3",
-      userName: "test1",
-    },
-    {
-      id: "4",
-      userName: "test2",
-    },
-  ];
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [search, setSearch] = useState("");
 
-  const [name, setName] = useState(users);
+  useEffect(() => {
+    fetchData();
+    return () => {};
+  }, []);
 
-  // const searchFn = (name) => {
-  //   const updatedName = users.filter((item)=> {const item_data = `${item.title.toUpperCase()})`;
-  //   const text_data = text.toUpperCase();
-  //   return item_data.indexOf(text_data) > -1;})
-  // }
-  // }
+  const fetchData = () => {
+    let unsubscribed = false;
+    const Uref = collection(db, "users");
+    const userDoc = getDocs(Uref)
+      .then((querySnapshot) => {
+        if (unsubscribed) return;
+        const newUserDataArray = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setData(newUserDataArray);
+        console.log(newUserDataArray);
+      })
+      .catch((err) => {
+        if (unsubscribed) return;
+        console.error("Failed to retrieve data", err);
+      });
+    return () => (unsubscribed = true);
+  };
 
-  renderPost = (user) => {
-    const selectPost = () => {
-      setId(user.id);
-      console.warn(user.id);
-    };
+  const searchFilter = (text) => {
+    if (text) {
+      const newData = data.filter((item) => {
+        const itemData = item.username
+          ? item.username.toUpperCase()
+          : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+      setSearch(text);
+    } else {
+      setFilteredData(data);
+      setSearch(text);
+    }
+  };
 
+  const ItemView = ({ item }) => {
+    return <Text>{item.username}</Text>;
+  };
+
+  const ItemSeparatorView = () => {
     return (
-      <View style={styles.item}>
-        <Text> {user.userName} </Text>
-      </View>
+      <View
+        style={{ height: 0.5, width: "100%", backgroundColor: "#C8C8C8" }}
+      />
     );
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Explore</Text>
-      {/* <InputBox value={name} setValue={setName} placeholder="Search" />       */}
-
-      <SearchBar
-        inputStyle={{ backgroundColor: "white" }}
-        containerStyle={{
-          backgroundColor: "white",
-          borderColor: "#f2f2f2",
-          borderWidth: 1,
-          borderRadius: 5,
-          height: "23%",
-        }}
-        placeholderTextColor={"#g5g5g5"}
-        inputContainerStyle={{ backgroundColor: "white", height: "10%" }}
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TextInput 
+        style={styles.searchBox}
         placeholder="Search"
-        lightTheme
-        round
-        value={name}
-        onChangeText={setName}
-        autoCorrect={false}
-      />
-      <ScrollView>
-      <FlatList
-        style={styles.list}
-        data={users}
-        renderItem={({ item }) => renderPost(item)}
-        keyExtractor={(item) => item.id}
-        showsVerticalScrollIndicator={false}
-      />
-      </ScrollView>
+          value={search}
+          onChangeText={(text) => searchFilter(text)}/>
+        <FlatList
+          data={filteredData}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          renderItem={ItemView}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default Explore;
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#ffffff",
-  },
-  item: {
-    backgroundColor: "white",
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: "#F2f2f2",
-    padding: 10,
-    margin: 1,
-  },
-});
+const styles = StyleSheet.create({});
